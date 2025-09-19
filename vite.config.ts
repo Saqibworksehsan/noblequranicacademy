6af5896cb19ac1b26af9f43ac3c453ef782e5915
Saqibworksehsan <saqibworksehsan@gmail.com>
2025-09-19 07:12:45 +0530

    latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/noblequranicacademy/',   // 👈 must match repo name
+  base: '/',   // 👈 must match repo name
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
